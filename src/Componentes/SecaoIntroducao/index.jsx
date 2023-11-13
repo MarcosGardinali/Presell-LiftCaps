68f7e5d17437369464cf400a691d60d49d76add8
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-11-13 09:44:48 -0300

    .

--- src/Componentes/SecaoIntroducao/index.jsx
+++ src/Componentes/SecaoIntroducao/index.jsx
@@ -9,7 +9,7 @@ export default function SecaoIntroducao() {
         <img className={style.conteudo__secao__introducao__imagem} src={logo} alt="Imagem Logo" />
         <h2 className={style.conteudo__secao__introducao__titulo}>Aproveite Nossos <strong>Super Descontos</strong> Nas Compras Acima de 2 Potes</h2>
         <p className={style.conteudo__secao__introducao__subtitulo}>Já estamos no final do ano e se você ainda não está confortável com seu corpo, <strong>ASSISTA O VÍDEO ABAIXO</strong> que iremos te ajudar a emagrecer.</p>
-        <iframe className={style.conteudo__secao__introducao__video} frameBorder="0" allowFullScreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" title="Lift Detox Black Como Funciona" src="https://www.youtube.com/embed/i7FCU51VQDU?controls=0&amp;rel=0&amp;playsinline=0&amp;showinfo=0&amp;modestbranding=0&amp;autoplay=&amp;enablejsapi=1&amp;"></iframe>
+        <iframe className={style.conteudo__secao__introducao__video} frameBorder="0" allowFullScreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" title="Lift Detox" src="https://www.youtube.com/embed/i7FCU51VQDU?controls=0&amp;rel=0&amp;playsinline=0&amp;showinfo=0&amp;modestbranding=0&amp;autoplay=&amp;enablejsapi=1&amp;"></iframe>
         <Botao>
             Continuar!
         </Botao>
